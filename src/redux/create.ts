ea45c95e48c2ada9d97f96d10d1efa91ab5d16fe
seo-png <iseohyeon128@gmail.com>
2023-05-06 23:51:21 +0900

    rudux 설정

---
+++ src/redux/create.ts
@@ -0,0 +1,20 @@
+import { applyMiddleware, legacy_createStore as createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducer from './modules/reducer';
+import createSagaMiddleware from '@redux-saga/core';
+import rootSaga from './modules/rootSaga';
+
+const create = () => {
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
+    reducer, 
+    composeWithDevTools(applyMiddleware(sagaMiddleware))
+  );
+
+  sagaMiddleware.run(rootSaga);
+
+  return store;
+};
+
+export default create;
