b79546253c087fb2d69f67ce4086f0a5f45d6ca6
seo-png <iseohyeon128@gmail.com>
2023-05-07 04:32:04 +0900

    sign in page 기본 ui

--- src/components/Signin.tsx
+++ src/components/Signin.tsx
@@ -1,7 +1,42 @@
+import { Button, Col, Row } from "antd";
+import Input from "rc-input";
+
 export default function Signin() {
   return (
-    <div>
-      <h1>Signin</h1>
-    </div>
+    <Row align="middle">
+      <Col span={24}>
+        <Row>
+          <Col span={12}></Col>
+          <Col span={12}>
+            <div>My Books</div>
+            <div>Please Note Your Opinion</div>
+            <div />
+            <div>
+              Email
+              <span> *</span>
+            </div>
+            <div>
+              <Input
+                placeholder="Email"
+                autoComplete="email"
+                name="email" />
+            </div>
+            <div>
+              Password
+              <span> *</span>
+            </div>
+            <div>
+              <Input
+                type="password"
+                autoComplete="current-password"
+                name="email" />
+            </div>
+            <div>
+              <Button size="large">Sign In</Button>
+            </div>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   )
 }
